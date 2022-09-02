2a2dd3875a7c82c918faecfdab86aaa40f7801fb
feifei <73841560+feiyin29@users.noreply.github.com>
2022-09-02 09:17:41 +0700

    complete

--- src/customermailapplication/CustomerMailApplication.java
+++ src/customermailapplication/CustomerMailApplication.java
@@ -19,17 +19,7 @@ public class CustomerMailApplication {
      */
     private Customer customer;
     public void getCustomerTypeFromUser(String customerType) {
-        switch(customerType) {
-            case "Regular":
-                customer = new RegularCustomer();
-                break;
-            case "Mountain":
-                customer = new MountainCustomer();
-                break;
-            case "Delinquent":
-                customer = new DelinquentCustomer();
-                break;
-        }
+        customer = CustomerFactory.createCustomer(customerType);
     }
     public String generateMail() {
         return customer.createMail();
